77dcba100371788847766f22a4e70802ac13fcfa
raphael <raph.koh@gmail.com>
2017-07-13 13:00:23 -0400

    Update CS 246 notes

--- notes/CS246/design.cc
+++ notes/CS246/design.cc
@@ -63,6 +63,7 @@ Controller
 - may encapsulate turn-taking or entire game rules (tradeoff with model)
 - may communicate with user for input or view
 
+
 Exception Safety
 void f() {
   MyClass *p = new MyClass;
@@ -128,5 +129,122 @@ void f() {
   } // p2 is popped bu object is NOT deleted
 }
 
+- Shared pointers will only delete the pointer once the count of shared pointers is zero
+
 Advice:
 Use shared_ptr and unique_ptr instead of raw pointers as much as possible.  They will dramatically reduce memory leaks
+
+Three levels of exception safety:
+1. Basic Guarantee
+  - If exception occurs, the program is valid, but in an unspecified state
+  - Nothing is leaked
+  - Class invariant are maintained
+  - e.g. Regular copy constructor where data is lost
+2. Strong Guarantee
+  - Exception is raised while executing error
+  - The state of the program will be as if the function had not been called
+  - e.g. Copy and Swap idiom
+3. Nothrow Guarantee
+  - Function will never throw an exception
+  - no exception is emitted outside of the function
+  - Always accomplishes its task
+  - use noexcept
+
+e.g.
+class A {}
+class B {}
+class C {
+  A a;
+  B b;
+public:
+  void f() {
+    a.method1();
+    b.method2();
+  }
+};
+- This is a basic guarantee.
+- If a.method1() runs and b.method2() has an error, the state of a is changed without b changing
+- Results in a "partial function"
+
+// To Provide Strong Exception Guarantee
+- Use Copy & Swap Idiom
+class C {
+  A a;
+  B b;
+public:
+  void f() {
+    A aTemp = a;  // calls copy ctor
+    B bTemp = b;
+    aTemp.method1();
+    bTemp.method2();
+    a = aTemp;
+    b = bTemp;
+  }
+}
+- Because copy assignment operator could throw, we dont yet have strong exception guarantee
+- Better if we could guarantee that the swap part is a nothrow
+- A non-throwing 'swap' operation is at the heart of writing exception-safe code
+- Key observation: Copying pointers cannot throw
+  - Solution: Use pImpl Idiom
+
+struct Cimpl {
+  A a;
+  B b;
+}
+class C {
+  unique_ptr<CImpl> pImpl;  // pointer to implementation
+public:
+  auto temp = make_unique<CImpl>(*pImpl);
+  temp->a.method1();
+  temp->b.method2();
+  std::swap(pImpl, temp);
+};  // Nothrow
+
+Exception Safety and the STL vectors
+- encapsulate heap-allocated array
+- RAII when stack-allocated vector goes out of scope, the internal heap-allocated array is free
+void f() {
+  vector<MyClass> v;
+}
+- when v goes out of scope, MyClass dtor runs on all items in the array and the array is freed
+void g(){
+  vector<MyClass*> v;
+}
+- when v goes out of scope, pointers dont have dtors so only the array is freed while pointers are not
+void g(){
+  vector<MyClass*> v;
+  for(auto &x: v) delete x;
+}
+// OR
+void h() {
+  vector<shared_ptr<MyClass>> v;
+}
+- when v goes out of scope, shared ptr dtors free objects once count of shared ptrs goes to zero
+
+vector<T?::emplace_back: offers strong guarantee
+- if array is full, it allocates a new, larger array
+- copy the objects over (copy ctor)
+- if a copy ctor throws:
+  - destroys new array
+  - old array is intact
+  - strong guarantee
+- else delete old array
+
+- Allocate new larger array
+- move the objects over
+- delete old array
+
+The problem:
+- if move ctor were to throw, the vector<T>::emplace_back wouldnt offer strong guarantee because the original array would no longer be intact.
+- But, emplace_back promises the strong guarantee.
+- Therefore, if the move offers the nothrow guarantee, emplace_back will use the move ctor otherwise it will use the copy ctor which may be slower
+- Move operations should provide nothrow guarantee if possible and should indicate that they do so
+class MyClass {
+public:
+  MyClass(MyClass &&other) noexcept {}
+  MyClass &operator=(MyClass &&other) noexcept {}
+}
+- If you know a function will never throw or propagate an exception, declare it noexcept
+  - Will facilitate optimization of your code
+  - Move & Swap should be noexcept
+- In general, exceptions > error codes for program design
